6c9679b0e294c1febc620c6a903405ecefdd0b94
Paradise <shrivastavanirmal24@gmail.com>
2026-03-02 17:25:38 +0530

    some home page layout

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  const handleSuggestionClick = (suggestion) => {
+    if(activeField  === 'pickup'){
+      setPickup(suggestion)
+    }
+  }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LocationSearchPanel
